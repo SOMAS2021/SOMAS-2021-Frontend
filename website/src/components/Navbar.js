10d442a9f1db6bf889f7ee3fab43b8079e4b5a85
JaafarRammal <jarammal@gmail.com>
2021-12-16 13:50:37 +0000

    remove unused

--- website/src/components/Navbar.js
+++ website/src/components/Navbar.js
@@ -1,12 +1,11 @@
 import React, {useState, useEffect} from 'react';
 import {Link} from 'react-router-dom';
-import { Button } from './Button';
 import '../style/Navbar.css';
 
 function Navbar() {
     const [click, setClick] = useState(false);
 
-    const [button, setButton] = useState(true);
+    const [_, setButton] = useState(true);
 
     const handleClick = () => setClick(!click);
 
