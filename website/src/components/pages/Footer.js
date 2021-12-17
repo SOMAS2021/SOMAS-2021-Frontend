981d18eea240bf651cb4e48a7d613152f7a4ebb6
suo18 <77544834+suo18@users.noreply.github.com>
2021-12-17 11:16:06 +0000

    Added Footer component to home

---
+++ website/src/components/pages/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../../style/Footer.css';
+
+function Footer() {
+    return (
+        <div className='footer-container'>
+            <h1>Imperial College London Electrical Engineering Department</h1>
+            <p> Self Organising Multi Agent Systems</p>
+        </div>
+    )
+}
+
+export default Footer;
