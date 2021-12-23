67d7cc2b4f2dc721fdfabcdb92a738029c2d6489
suo18 <77544834+suo18@users.noreply.github.com>
2021-12-23 10:16:17 +0100

    Added footer & styling

--- website/src/components/pages/Footer.js
+++ website/src/components/pages/Footer.js
@@ -4,8 +4,13 @@ import '../../style/Footer.css';
 function Footer() {
     return (
         <div className='footer-container'>
-            <h1>Imperial College London Electrical Engineering Department</h1>
+            <h1>Imperial College London Electrical & Electronic Engineering Department</h1>
             <p> Self Organising Multi Agent Systems</p>
+            <div className='social-icons'>
+            <a href='https://github.com/SOMAS2021' className="icon-holder" target='_blank'>
+                Github <i class='fab fa-github' />
+            </a>
+            </div>
         </div>
     )
 }
