2ab07ec6cd7db1c8c559105511ee3efe0beab037
suo18 <77544834+suo18@users.noreply.github.com>
2021-12-29 00:24:13 +0100

    Cganged website structure

--- website/src/components/Hero.js
+++ website/src/components/Hero.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../App.css';
 import '../style/Hero.css';
-import Video from '../videos/platform.mov';
+import { Button } from '../components/Button';
+import Modal from './Modal';
+import Video from '../videos/platform2.mov';
 
 function Hero() {
+  const [modal, setModal] = useState(false);
+  const Toggle = () => setModal(!modal);
   return (
-    <div className="hero-container">
-      <video src={Video} autoPlay loop muted />
-      <h1>SELF ORGANISING MULTI AGENT SYSTEMS 2021</h1>
-      <p>The Platform-Playing Platform</p>
-    </div>
+      <div className="hero-container">
+        {/* <video src={Video} autoPlay loop muted /> */}
+        {/* <image src={Image}/> */}
+        <h1>SELF ORGANISING MULTI AGENT SYSTEMS 2021</h1>
+        <p>The Platform-Playing Platform</p>
+        <Button
+          className='clickme'
+          buttonStyle='btn--outline'
+          buttonSize='btn--large'
+          onClick={() => Toggle()}
+        >
+          WATCH ANIMATION <i className='far fa-play-circle' />
+        </Button>
+        <Modal show={modal} title="Platform Animation" close={Toggle}/>
+      </div>
   );
 }
 
