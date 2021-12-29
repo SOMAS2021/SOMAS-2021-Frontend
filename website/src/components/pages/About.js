f9dcc5eb199a0a6e22f344052cbd9bf9dc057032
suo18 <77544834+suo18@users.noreply.github.com>
2021-12-29 02:29:29 +0100

    Changed Website Structure

--- website/src/components/pages/About.js
+++ website/src/components/pages/About.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import'../../App.css'
 import AboutCards from '../AboutCards'
+import Navbar from '../Navbar';
+import Footer from './Footer';
 
 function About() {
     return (
-        <div>
-           <h1 className='about'>ABOUT SOMAS 2021</h1>;
+        <>
+            <div>
+            <Navbar/>
+            <h1 className='about'>ABOUT SOMAS 2021</h1>;
+            </div>
             <AboutCards />
-        </div>
+            <Footer/>
+        </>
     );
 }
 
