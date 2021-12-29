3898bf1aa8a166d418e9d28e85f8911780ced615
suo18 <77544834+suo18@users.noreply.github.com>
2021-12-29 02:36:58 +0100

    Fixed warnings

--- website/src/App.js
+++ website/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
--- website/src/components/Button.js
+++ website/src/components/Button.js
@@ -1,19 +1,5 @@
 import React from 'react';
 import '../style/Button.css';
-import { Link } from 'react-router-dom';
-//import styled from 'styled-components';
-
-
-
-/*const Button = styled.button`
-  background-color: black;
-  color: white;
-  font-size: 20px;
-  padding: 10px 60px;
-  border-radius: 5px;
-  margin: 10px 0px;
-  cursor: pointer;
-`;*/
 
 const STYLES = ['btn--primary', 'btn--outline'];
 
